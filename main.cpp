57dad7ad531f593503a1427fcb9c2a9b806daca3
cesar <camgcamg11@gmail.com>
2020-09-25 22:00:17 -0500

    missing main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,4 @@
 #include "btree.h"
-#include "iostream"
-using namespace std;
 
 int main(){
     srand (time(NULL));
@@ -8,14 +6,24 @@ int main(){
     int n, temp;
     std::cin>>n>>ORDER;
     btree<int> bt;
+    
     for (int i = 0; i < n; i++) {
-        //cin>>temp;
-        temp = rand() % 100 + 1;
-        cout<<temp<<" ";
+        cin>>temp;
         bt.insert(temp);
     }
-    cout<<endl;
+
+    
+    
     bt.print();
+    for (int i=0;i<2;i++){
+        cin>>temp;
+        bt.delete_index(temp);
+        bt.print();
+    }
+    
     bt.printLeafs();
     return 0;
 }
+//10 3 
+//1 4 3 7 9 5 8 2 10 6
+//6 5 
